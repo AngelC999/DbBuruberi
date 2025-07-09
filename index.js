7dcd6ec5cd7d9c1e885a5c02a8c7dcc08da0cc69
AngeloC12 <angelocuri@hotmail.com>
2025-07-09 01:33:55 -0500

    fg
    fgfgf

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
+const path = require('path');
+
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
-    console.log('JSON Server is running');
-});
+
+// Exportar como handler para Vercel
+module.exports = server;
